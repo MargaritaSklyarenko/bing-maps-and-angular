314a8def4fcaa15ac19ce8806068aee70b76a78f
Marharyta Skliarenko <marharyta.skliarenko@MediaKind.com>
2019-08-30 11:58:10 +0300

    Working with errors

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { Microsoft } from ''
 
 @Component({
   selector: 'app-map',
@@ -8,14 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class MapComponent implements OnInit {
 
   private map;
+  
   constructor() { }
 
   ngOnInit() {  
-    this.loadMapScenario();
+    this.loadMap();
   }
 
-  loadMapScenario() {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+  loadMap() {
+    if (document.getElementById("scriptBingMaps")) {
+      return; // already loaded
+    }
+
+    // Add a global function for the callback from Bing Maps api
+    (<any>window).OnLoadBingMapsApi = () => this.InitMap();
+
+    // Add programmaticaly the external Bing maps api script
+    var scriptTag = document.createElement("script");
+    scriptTag.src = "https://www.bing.com/api/maps/mapcontrol?callback=OnLoadBingMapsApi";
+    scriptTag.id = "scriptBingMaps";
+    // Inject the dynamic script in the DOM
+    document.head.appendChild(scriptTag);
+  }
+
+
+  private InitMap(): void {
+    let mapElement: HTMLElement = <HTMLElement>document.getElementById('myMap');
+    var map = new Microsoft.Maps.Map(mapElement, {});
   }
 
 }
